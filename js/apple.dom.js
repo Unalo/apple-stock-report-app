eb1ccdc3b09a7400a169573dcf4363dd325900a4
unalo <unalomtshokotsha@gmail.com>
2022-06-05 23:56:43 +0200

    added handlebars template

--- js/apple.dom.js
+++ js/apple.dom.js
@@ -5,8 +5,8 @@ let profitElement = document.querySelector(".profit");
 let messageElement = document.querySelector(".message");
 let calculateBtn = document.querySelector(".btn");
 
-let previouse = localStorage.getItem("dataList") ? JSON.stringify(localStorage.getItem("dataList")) : [];
-console.log(previouse);
+let previouse = localStorage.getItem("dataList") ? JSON.parse(localStorage.getItem("dataList")) : [];
+// console.log(previouse);
 
 let apples = appleFactory(previouse);
 
@@ -34,7 +34,8 @@ const calculations = () => {
         const dateStamp = currentDateTime.toLocaleDateString();
 
         M.toast({ html: 'Data captured successfully', classes: 'green darken-5 rounded', displayLength: '1000' })
-        messageElement.innerHTML = `You will be able to make ${packets} packets and can be sold for R${profitMoney} per pack at ${moneyProfit}%. One apple will cost R${appleCost}, Your profit will be R${profitMoney}`
+        messageElement.innerHTML = `You will be able to make ${packets} packets and can be sold for R${profitMoney} per pack at ${moneyProfit}%. One apple will cost R${appleCost}, Your profit will be R${profitMoney}`;
+
         setTimeout(() => {
             messageElement.innerHTML = "";
         }, "10000");
@@ -43,14 +44,25 @@ const calculations = () => {
             packets, profitMoney, moneyProfit, appleCost, profitMoney, timeStamp, dateStamp
         });
 
+        console.log(apples.getDataList());
         localStorage.setItem('dataList', JSON.stringify(apples.getDataList()));
+
     } else {
         M.toast({ html: 'Please fill all the fields', classes: 'red darken-5 rounded', displayLength: '2000' })
     }
 }
 
 let previouseData = () => {
-    
+
 }
 
+const renderHello = ()=> {
+    // console.log(previouse);
+    // let list = [{"packets":"1","profitMoney":"0.01","moneyProfit":"1","appleCost":"5.00","timeStamp":"22:40:24","dateStamp":"05/06/2022"}]
+    var template = document.getElementById('template').innerHTML;
+    var rendered = Mustache.render(template, {previouse});
+    document.getElementById('target').innerHTML = rendered;
+  }
+
 calculateBtn.addEventListener("click", calculations);
+
